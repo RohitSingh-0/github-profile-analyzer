452ca26fa59a06656f4b90e45aceab29611cfb56
Rohit <0rohitrajput00@gmail.com>
2026-06-06 23:14:22 +0530

    Add health endpoint

--- src/server.js
+++ src/server.js
@@ -17,3 +17,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
+});
